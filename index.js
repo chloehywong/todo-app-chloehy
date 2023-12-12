b93935f554bea3f38d7dcb98f626b0664b466f57
Chloe Wong <wonghoyee0114@gmail.com>
2023-12-12 16:01:03 +0000

    add update()

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 let taskData = [];
-let taskDoneData = [];
+const todoForm = document.getElementById('todo-form');
+const taskInput = document.getElementById('task-input');
+
+
+document.addEventListener('input', (e) => {
+    if (e.target.dataset.update) {
+        const taskId = e.target.closest('div').id
+        updateTask(taskId, e.target)
+    }
+})
+
+
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addTask()
+})
 
 document.addEventListener("click", stateHandle);
 
@@ -18,11 +33,10 @@ function stateHandle(e) {
 
 
 function addTask() {
-    const taskInput = document.getElementById('task-input')
-
     if (taskInput.value) {
         taskData.unshift({
             title: taskInput.value,
+            description: 'Add description',
             isComplete: false,
             uuid: uuidv4()
         })
@@ -36,11 +50,6 @@ function deleteTask(taskId) {
         return task.uuid === taskId
     });
     taskData.splice(index1, 1)
-     
-    var index2 = taskDoneData.findIndex(function (task) {
-        return task.uuid === taskId
-    });
-    taskDoneData.splice(index2, 1)
 
     render()
 }
@@ -53,66 +62,63 @@ function completeTask(taskId) {
 
     targetTaskObj.isComplete = !targetTaskObj.isComplete
 
-    taskDoneData.push(targetTaskObj)
     render()
 }
 
-
-function getTodoTaskListHtml() {
-    let todoHtml = ''
-
-    if (taskData.length === 0 && taskDoneData.length === 0) {
-        todoHtml += `
-            <h2>Are you planning to chill today? if not, add some tasks :)</h2>
-            `
+function updateTask(taskId, el) {
+    var task = taskData.find((task) => task.uuid === taskId);
+    if (el.classList.contains("task-comment-area")) {
+        task.description = el.textContent
     } else {
-        todoHtml = `<h2 class='session-title'>To-do</h2>`
-        for (let task of taskData) {
-            if (!task.isComplete) {
-                todoHtml += `
-                    <div id="${task.uuid}" class='task'>
-                        <button class="complete-btn" data-complete='${task.uuid}'></button>
-                        <li class='text-task'>${task.title}</li>
-                        <textarea class='task-comment-area' placeholder='Description/Comment...'></textarea>
-                        <button class='close-btn' data-delete='${task.uuid}'>x</button>
-                    </div>
-                    `
-            }
-
-        }
+        task.title = el.textContent
     }
-    console.log(taskData)
-    return todoHtml
 }
 
 
-function getCompleteTaskListHtml() {
-    let doneHtml = ''
-    if (taskDoneData.length > 0) {
-        doneHtml = `<h2 class='session-title'>Done</h2>`
-        for (let done of taskDoneData) {
-            let completeColour = ''
-            if (done.isComplete) {
-                completeColour = "pink"
-            }
-            doneHtml += `
-            <div id="${done.uuid}" class='task'>
-                <button class="complete-btn" data-complete='${done.uuid}' style='background-color:${completeColour}'></button>
-                <li class='text-task'>${done.title}</li>
-                <textarea class='task-comment-area' placeholder='Description/Comment...'></textarea>
-                <button class='close-btn' data-delete='${done.uuid}'>x</button>
+function getTaskListHtml() {
+    let html = ''
+    let completeColour = ''
+
+    if (taskData.length === 0) {
+        html += `
+        <h2>Are you planning to chill today? if not, add some tasks :)</h2>
+        `
+    } else {
+        // Render the todo tasks
+        let todoTasks = taskData.filter((task) => {return task.isComplete === false})
+        html += `<h2 class='session-title'>To-do</h2>`
+        for (let task of todoTasks) {
+            html += `
+            <div id="${task.uuid}" class='task'>
+                <button class="complete-btn" data-complete='${task.uuid}'></button>
+                <span contenteditable class='text-task' data-update='${task.uuid}'>${task.title}</span>
+                <span contenteditable class='task-comment-area' data-update='${task.uuid}'>${task.description}</span>
+                <button class='close-btn' data-delete='${task.uuid}'>x</button>
+            </div>
+            `
+        }
+    
+        // Render the done tasks
+        let doneTasks = taskData.filter((task) => {return task.isComplete === true})
+        html += `<h2 class='session-title'>Done</h2>`
+        for (let task of doneTasks) {
+            completeColour = "pink"
+            html += `
+            <div id="${task.uuid}" class='task'>
+                <button class="complete-btn" data-complete='${task.uuid}' style='background-color:${completeColour}'></button>
+                <span contenteditable class='text-task' data-update='${task.uuid}'>${task.title}</span>
+                <span contenteditable class='task-comment-area' data-update='${task.uuid}'>${task.description}</span>
+                <button class='close-btn' data-delete='${task.uuid}'>x</button>
             </div>
             `
         }
     }
-    console.log(taskDoneData)
-    return doneHtml
+    return html
 }
 
 
 function render() {
-    document.getElementById('task-list-todo').innerHTML = getTodoTaskListHtml()
-    document.getElementById('task-list-done').innerHTML = getCompleteTaskListHtml()
+    document.getElementById('task-list').innerHTML = getTaskListHtml()
 }
 
 render()
